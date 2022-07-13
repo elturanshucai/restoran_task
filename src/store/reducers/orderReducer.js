412376e10f4b1a60841851c59504f59fde50d0b2
elturanshucai <96484306+elturanshucai@users.noreply.github.com>
2022-07-13 11:20:11 +0400

    Add files via upload

--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -7,11 +7,15 @@ export const orderSlice = createSlice({
         orderBasket: [],
         total: 0,
         orders: [],
+        trueOrders : [],
+        falseOrders: [],
+        myorder: {},
         sign: false,
         admin: {
             email: 'elturanfcb@gmail.com',
             password: '12345'
         },
+        quantity: 0,
     },
     reducers: {
         setBasket: (state, { payload }) => {
@@ -21,22 +25,28 @@ export const orderSlice = createSlice({
                 return acc + (item.quantity * item.price)
             }, 0)
         },
-        setOrders: (state) => {
+        setOrders: (state, {payload}) => {
             let orders = JSON.parse(localStorage.getItem('orders'))
-            if (JSON.parse(localStorage.getItem('orders'))) {
+            if (orders) {
                 state.orders = orders
             }
-            state.orders = [...state.orders, state.basket]
+            state.myorder=payload
+            state.orders=[...state.orders, state.myorder]
             localStorage.setItem('orders', JSON.stringify(state.orders))
+            state.total = 0
             state.basket = []
+        },
+        trueSuccess: (state, {payload})=>{
+            state.orders[payload].success=true
+            localStorage.setItem('orders', JSON.stringify(state.orders))
             state.total = 0
         },
         setLocalOrders: (state, { payload }) => {
             state.orders = payload
         },
         removeOrder: (state, { payload }) => {
-            state.orders = state.orders.filter((item, index) => (index !== payload))
-            localStorage.setItem('orders', JSON.stringify(state.orders))
+                state.orders = state.orders.filter((item, index) => (index !== payload))
+                localStorage.setItem('orders', JSON.stringify(state.orders))
         },
         cancelOrder: (state, { payload }) => {
             state.basket.map((item, index) => {
@@ -57,9 +67,12 @@ export const orderSlice = createSlice({
         },
         logout: () => {
             localStorage.removeItem('lgn')
+        },
+        setQuantity: (state, {payload})=>{
+            state.quantity=payload
         }
     }
 })
 
-export const { setBasket, setOrders, cancelOrder, setSign, login, logout, setLocalOrders, removeOrder} = orderSlice.actions
+export const { setBasket, setOrders, cancelOrder, setSign, login, logout, setLocalOrders, removeOrder, setQuantity, trueSuccess} = orderSlice.actions
 export default orderSlice.reducer
